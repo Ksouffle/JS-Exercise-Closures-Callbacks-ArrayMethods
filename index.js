dca5c54330648a6ebc0856e621de570e8574eab8
Ethan Karp <ethansmainemail@gmail.com>
2020-10-28 18:20:20 -0600

    finished task 3

--- index.js
+++ index.js
@@ -63,14 +63,14 @@ function counter2() {
 
 Write a function called `inning` that generates a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(team){
+function inning(){
 
     let rnumber = Math.floor((Math.random()) * 3);
-    return (`The ${team} scorred ${rnumber} points`);
+    return rnumber;
 
 }
 
-console.log(inning("Yankies"));
+console.log(inning());
 
 /* Task 3: finalScore()
 
@@ -86,11 +86,16 @@ finalScore(inning, 9) might return:
 
 */ 
 
-function finalScore(/*code Here*/){
+function finalScore(callback, innNum){
 
-  /*Code Here*/
+  return {
+    "Home": callback() + innNum,
+    "Away": callback() + innNum
+  }
 
 }
+
+console.log(finalScore(inning, 9));
 
 /* Task 4: 
 
